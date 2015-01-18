a0614068812b6d484c5a23ff66e50e72bc002c55
TREY <rkh.mymail@gmail.com>
2015-01-18 14:52:29 -0500

    add station_controller

---
+++ app/controllers/station_controller.rb
@@ -0,0 +1,15 @@
+class StationController < ApplicationController
+  def show
+		@stationJSON = HTTParty.get('http://www.capitalbikeshare.com/data/stations/bikeStations.xml')
+  end
+
+  def search
+  	id = params[:q]
+  	redirect_to station_show_path(id)
+  end
+
+  def show
+  	@id = params[:id]
+  	@station = Station.find(@id)
+  end
+end
